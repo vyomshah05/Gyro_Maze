883a88d92fc36737a01628b4c707830cb252a81b
Vyom Shah <75156655+vyomshah05@users.noreply.github.com>
2025-09-01 16:15:38 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,40 +9,13 @@
 #include <ArduinoJson.h>
 #include <string>
 // Wi-Fi credentials
-#define WIFI_SSID "Vyomphone"         // NOTE: Please delete this value before submitting assignment
-#define WIFI_PASSWORD "48174817" // NOTE: Please delete this value before submitting assignment
+#define WIFI_SSID ""         // NOTE: Please delete this value before submitting assignment
+#define WIFI_PASSWORD "" // NOTE: Please delete this value before submitting assignment
 // Azure IoT Hub configuration
-#define SAS_TOKEN "SharedAccessSignature sr=cs147group.azure-devices.net%2Fdevices%2F147esp32&sig=Y935uKZ35%2BVJWWTOP10q%2FVPz1GCQfbzWYDp%2FbgABnjQ%3D&se=2356418040"
-const char *root_ca = "-----BEGIN CERTIFICATE-----\n"\
-"MIIEtjCCA56gAwIBAgIQCv1eRG9c89YADp5Gwibf9jANBgkqhkiG9w0BAQsFADBh\n"\
-"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"\
-"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n"\
-"MjAeFw0yMjA0MjgwMDAwMDBaFw0zMjA0MjcyMzU5NTlaMEcxCzAJBgNVBAYTAlVT\n"\
-"MR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xGDAWBgNVBAMTD01TRlQg\n"\
-"UlMyNTYgQ0EtMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMiJV34o\n"\
-"eVNHI0mZGh1Rj9mdde3zSY7IhQNqAmRaTzOeRye8QsfhYFXSiMW25JddlcqaqGJ9\n"\
-"GEMcJPWBIBIEdNVYl1bB5KQOl+3m68p59Pu7npC74lJRY8F+p8PLKZAJjSkDD9Ex\n"\
-"mjHBlPcRrasgflPom3D0XB++nB1y+WLn+cB7DWLoj6qZSUDyWwnEDkkjfKee6ybx\n"\
-"SAXq7oORPe9o2BKfgi7dTKlOd7eKhotw96yIgMx7yigE3Q3ARS8m+BOFZ/mx150g\n"\
-"dKFfMcDNvSkCpxjVWnk//icrrmmEsn2xJbEuDCvtoSNvGIuCXxqhTM352HGfO2JK\n"\
-"AF/Kjf5OrPn2QpECAwEAAaOCAYIwggF+MBIGA1UdEwEB/wQIMAYBAf8CAQAwHQYD\n"\
-"VR0OBBYEFAyBfpQ5X8d3on8XFnk46DWWjn+UMB8GA1UdIwQYMBaAFE4iVCAYlebj\n"\
-"buYP+vq5Eu0GF485MA4GA1UdDwEB/wQEAwIBhjAdBgNVHSUEFjAUBggrBgEFBQcD\n"\
-"AQYIKwYBBQUHAwIwdgYIKwYBBQUHAQEEajBoMCQGCCsGAQUFBzABhhhodHRwOi8v\n"\
-"b2NzcC5kaWdpY2VydC5jb20wQAYIKwYBBQUHMAKGNGh0dHA6Ly9jYWNlcnRzLmRp\n"\
-"Z2ljZXJ0LmNvbS9EaWdpQ2VydEdsb2JhbFJvb3RHMi5jcnQwQgYDVR0fBDswOTA3\n"\
-"oDWgM4YxaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0R2xvYmFsUm9v\n"\
-"dEcyLmNybDA9BgNVHSAENjA0MAsGCWCGSAGG/WwCATAHBgVngQwBATAIBgZngQwB\n"\
-"AgEwCAYGZ4EMAQICMAgGBmeBDAECAzANBgkqhkiG9w0BAQsFAAOCAQEAdYWmf+AB\n"\
-"klEQShTbhGPQmH1c9BfnEgUFMJsNpzo9dvRj1Uek+L9WfI3kBQn97oUtf25BQsfc\n"\
-"kIIvTlE3WhA2Cg2yWLTVjH0Ny03dGsqoFYIypnuAwhOWUPHAu++vaUMcPUTUpQCb\n"\
-"eC1h4YW4CCSTYN37D2Q555wxnni0elPj9O0pymWS8gZnsfoKjvoYi/qDPZw1/TSR\n"\
-"penOgI6XjmlmPLBrk4LIw7P7PPg4uXUpCzzeybvARG/NIIkFv1eRYIbDF+bIkZbJ\n"\
-"QFdB9BjjlA4ukAg2YkOyCiB8eXTBi2APaceh3+uBLIgLk8ysy52g2U3gP7Q26Jlg\n"\
-"q/xKzj3O9hFh/g==\n"\
-"-----END CERTIFICATE-----";
-String iothubName = "cs147group"; 
-String deviceName = "147esp32";     // Your device name (replace if needed)
+#define SAS_TOKEN ""
+const char *root_ca = "";
+String iothubName = ""; 
+String deviceName = "";     // Your device name (replace if needed)
 String url = "https://" + iothubName + ".azure-devices.net/devices/" +
              deviceName + "/messages/events?api-version=2021-04-12";
 // Telemetry interval
